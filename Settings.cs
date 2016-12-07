5a6964faef9772a2d21b0a03af2cde30d00c29d0
Chryssie Ta <Chryssie@users.noreply.github.com>
2016-12-07 21:32:24 +1100

    Fixed detection for Overwatch using GUID of the assembly. Update the in-game label.

--- Settings.cs
+++ Settings.cs
@@ -4,7 +4,7 @@
     {
         private Settings()
         {
-            Tag = "Remove Pigs [Fixed for v1.4+]";
+            Tag = "Remove Pigs [Fixed for v1.6]";
         }
 
         private static readonly Settings _Instance = new Settings();
